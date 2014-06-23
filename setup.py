f76ff603d1355d7b540dbc5655f6a589f6dc40c8
Natsu- <romain.garrigues.cs@gmail.com>
2014-06-23 22:55:03 +0200

    Adding setup.py project file

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-biolabs',
+    version='0.1',
+    author='Romain Garrigues',
+    author_email='romain.garrigues.cs@gmail.com',
+    url='http://youkidea.com',
+    description="Biolabs",
+    install_requires=[
+        'South == 0.8.4',
+        'django == 1.6',
+    ],
+    license='BSD, see LICENSE file.',
+    packages=find_packages(),
+    classifiers=['Natural Language :: English',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Programming Language :: Python :: 2.7'],
+)
