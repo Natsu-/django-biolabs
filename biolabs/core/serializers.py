ac3245b871e30e7edc1d8663962297c072934bb0
Natsu- <romain.garrigues.cs@gmail.com>
2014-07-13 19:27:44 +0200

    Lat/lng fields are added to api results

--- biolabs/core/serializers.py
+++ biolabs/core/serializers.py
@@ -5,4 +5,5 @@ from rest_framework import serializers
 class LaboratorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = core_models.Laboratory
-        fields = ('name', 'description', 'url', 'adress')
+        fields = ('name', 'description', 'url', 'adress',
+                  'latitude', 'longitude')
