49d2b5211a15d2e09c0da68d17de78c74c16d383
Natsu- <romain.garrigues.cs@gmail.com>
2015-01-04 23:03:53 +0000

    Adding non required fields parameter for fields description, adress, latitude and longitude in Laboratory model serializer

--- biolabs/core/serializers.py
+++ biolabs/core/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 
 
 class LaboratorySerializer(serializers.HyperlinkedModelSerializer):
+    description = serializers.CharField(required=False, max_length=255)
+    adress = serializers.CharField(required=False, max_length=255)
+    latitude = serializers.DecimalField(required=False, max_digits=23, decimal_places=20)
+    longitude = serializers.DecimalField(required=False, max_digits=23, decimal_places=20)
+
     class Meta:
         model = core_models.Laboratory
         fields = ('name', 'description', 'url', 'adress',
